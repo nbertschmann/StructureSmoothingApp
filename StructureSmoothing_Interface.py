dd576dac5d605062fb9fc015838341b2a836f416
Noah Bertschmann <noah@attabotics.com>
2020-12-04 16:21:29 -0700

    Updated interface. Includes separate pages to analyze log files and to create surface plots

--- StructureSmoothing_Interface.py
+++ StructureSmoothing_Interface.py
@@ -5,41 +5,88 @@ from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication)
 from PyQt5 import QtWidgets
 import os
 from PyQt5.QtCore import QThreadPool
+from PyQt5.QtCore import QAbstractItemModel
+
+
 
 class StructureSmooth(QWidget):
     def __init__(self):
         super().__init__()
+        self.my_layout = QtWidgets.QVBoxLayout(self)
 
-        self.browse_button = QtWidgets.QPushButton('Browse')
-        self.browse_box = QtWidgets.QLineEdit()
-        self.currentLevelness_box = QtWidgets.QTabWidget()
-        self.newLevelness_Box  = QtWidgets.QTabWidget()
-        self.analyzeStructure_button = QtWidgets.QPushButton('Analyze Structure')
+        self.pushButton1 = QPushButton("Create Plots")
+        self.browse_button1 = QtWidgets.QPushButton('Browse')
+        self.browse_box1 = QtWidgets.QLineEdit()
 
-        self.currentLevelness_text = QtWidgets.QLabel('Current Structure')
-        self.newLevelness_text = QtWidgets.QLabel('New Structure')
+        self.plotDisplay_tab = QtWidgets.QTabWidget()
 
-        browse_layout = QtWidgets.QHBoxLayout()
-        browse_layout.addWidget(self.browse_button)
-        browse_layout.addWidget(self.browse_box)
+        self.browse_layout1 = QtWidgets.QHBoxLayout()
+        self.browse_layout1.addWidget(self.browse_button1)
+        self.browse_layout1.addWidget(self.browse_box1)
 
-        master_layout = QtWidgets.QVBoxLayout()
-        master_layout.addLayout(browse_layout)
-        master_layout.addWidget(self.currentLevelness_text)
-        master_layout.addWidget(self.currentLevelness_box)
-        master_layout.addWidget(self.newLevelness_text)
-        master_layout.addWidget(self.newLevelness_Box)
-        master_layout.addWidget(self.analyzeStructure_button)
+        self.tab_layout1 = QtWidgets.QVBoxLayout()
+        self.tab_layout1.addLayout(self.browse_layout1)
+        self.tab_layout1.addWidget(self.plotDisplay_tab)
+        self.tab_layout1.addWidget(self.pushButton1)
 
-        self.setLayout(master_layout)
+        self.pushButton2 = QtWidgets.QPushButton("Analyze Logs")
+        self.browse_button2 = QtWidgets.QPushButton('Browse')
+        self.browse_box2 = QtWidgets.QLineEdit()
+
+        self.table_display = self.initTable()
+
+        self.browse_layout2 = QtWidgets.QHBoxLayout()
+        self.browse_layout2.addWidget(self.browse_button2)
+        self.browse_layout2.addWidget(self.browse_box2)
+
+        self.tab_layout2 = QtWidgets.QVBoxLayout()
+        self.tab_layout2.addLayout(self.browse_layout2)
+        self.tab_layout2.addWidget(self.table_display)
+        self.tab_layout2.addWidget(self.pushButton2)
+
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+
+        self.tab1.layout = QtWidgets.QVBoxLayout(self)
+        self.tab1.layout.addLayout(self.tab_layout1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        self.tab2.layout = QtWidgets.QVBoxLayout(self)
+        self.tab2.layout.addLayout(self.tab_layout2)
+        self.tab2.setLayout(self.tab2.layout)
+
+        self.select_tab = QtWidgets.QTabWidget()
+        self.select_tab.addTab(self.tab1, 'Create Plots')
+        self.select_tab.addTab(self.tab2, 'Analyze Logs')
+
+        self.my_layout.addWidget(self.select_tab)
+        self.setLayout(self.my_layout)
         self.initUI()
 
-        self.connect()
+
+        # self.connect()
+
+    def initTable(self):
+
+        model = QtGui.QStandardItemModel()
+        model.setHorizontalHeaderLabels(['DM', 'X', 'Y', 'Z', 'Pitch', 'Roll'])
+        table = QtWidgets.QTableView()
+        table.setModel(model)
+
+        header = table.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(5, QtWidgets.QHeaderView.Stretch)
+
+        return table
 
     def initUI(self):
 
         self.setGeometry(50, 75, 1000, 900)
-        self.setWindowTitle('Smooooooth')
+        self.setWindowTitle('Structure Smoothness')
 
         self.show()
 
